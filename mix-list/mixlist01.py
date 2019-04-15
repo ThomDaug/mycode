210a50d08543e722b8bded38f02e8d8789ddcb01
TDaug <thomdaug@yahoo.com>
2019-04-15 18:03:25 -0400

    suck me

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+my_list=['192.168.0.5',5060,'up']
+print("first item is (IP): "+my_list[0])
+print("the rest of the crap "+str(my_list[1])+my_list[2])
+# yeah I know how to comment
